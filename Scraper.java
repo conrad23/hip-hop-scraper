c522949bcf05dcea7f49c414493cfbf356ae9a50
Conner Toney <conrad.toney@gmail.com>
2015-02-01 17:45:05 -0500

    get rid of duplicate info

--- Scraper.java
+++ Scraper.java
@@ -1,13 +1,10 @@
-/* 
- * HIP HOP SCRAPER
- * 
- * FEATURES THAT NEED TO BE ADDED:
- * add methods/expand from main method
- * implement GUI
- * auto-run daily, keeping track of releases
- * 
- */
+/**********************************************************************
+Has the ability to retrieve artist and track title content from
+the hiphopearly.com website.
 
+@author Conner Toney
+@version 1.1
+ *********************************************************************/
 
 package package1;
 
@@ -21,30 +18,39 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Scraper {
-
-	public static void main(String[] args) throws IOException {
-
-		//arraylists to hold artists and tracks
+	
+	/** contains list of released artists/tracks */
+	private static String released = "";
+	
+	/******************************************************************
+	Retrieves the content from the website, and compares that content
+	with the user-specified content in order to determine if any
+	matches exist.
+	 *****************************************************************/
+	public static void getContent() throws IOException {
+		
+		//arraylists used to hold artist/track information
 		ArrayList<String> artistList = new ArrayList<String>();
 		ArrayList<String> trackList = new ArrayList<String>();
+		ArrayList<String> faveArtistList = new ArrayList<String>();
 
-		//getting connection to website and loading source code
+		//connects to the website, and retrieves the section
+		//containing relevant information
 		String url = "http://www.hiphopearly.com/tracks";
 		Document doc = Jsoup.connect(url).header("User-Agent", "curl").get();
 		Elements tracks = doc.select("div.track-listing div.track");
 
-		//loops through elements and adds artists/tracks to list
+		//iterates through the information, grabbing the info
+		//containing artists + tracks
 		for (Element track : tracks) {
 			artistList.add(track.select("span.artist").text());
 			trackList.add(track.select("span.title").text());
 		}
 
-		//FAVORITE ARTIST SECTION
-		System.out.println("FAVORITE ARTISTS - NEW RELEASES");
-		ArrayList<String> faveArtistList = new ArrayList<String>();
+		//reads in the user-given artists
 		try {
-			FileReader inFile1 = new FileReader("favoriteArtists.txt");
-			BufferedReader bufReader = new BufferedReader(inFile1);
+			FileReader inFile = new FileReader("favoriteArtists.txt");
+			BufferedReader bufReader = new BufferedReader(inFile);
 			String listOfFaveArtists;
 			while ((listOfFaveArtists = bufReader.readLine()) != null) {
 				String[] tokens = listOfFaveArtists.split("/");
@@ -54,43 +60,24 @@ public class Scraper {
 			}
 			bufReader.close();
 		} catch (IOException e) {
-			System.out.println("Error retrieving fave artist file.");
+			released = "Error reading user's file.";
 		}
+		
+		//if the scraped artist matches the user-given artist, 
+		//the artist is added to the 'released' string
 		for (int i = 0; i < artistList.size(); i++) 
 			for (int j = 0; j < faveArtistList.size(); j++) {
 				if (artistList.get(i).toLowerCase().contains(faveArtistList.get(j))) {
-					System.out.println(artistList.get(i));
-					System.out.println(trackList.get(i));
-					System.out.println();
+					released += (artistList.get(i) + "- \n" + trackList.get(i) + "\n" + "\n");
 				}
 			}
-
-		System.out.println("-----------------------------------------");
-
-		//SIMILAR ARTIST SECTION
-		System.out.println("SIMILAR ARTISTS - NEW RELEASES");
-		ArrayList<String> simArtistList = new ArrayList<String>();
-		try {
-			FileReader inFile2 = new FileReader("similarArtists.txt");
-			BufferedReader bufReader = new BufferedReader(inFile2);
-			String listOfSimArtists;
-			while ((listOfSimArtists = bufReader.readLine()) != null) {
-				String[] tokens = listOfSimArtists.split("/");
-				for (int i = 0; i < tokens.length; i++) {
-					simArtistList.add(tokens[i]);
-				}
-			}
-			bufReader.close();
-		} catch(IOException e) {
-			System.out.println("Error retrieving similar artist file.");
-		}
-		for (int i = 0; i < artistList.size(); i++) 
-			for (int j= 0; j < simArtistList.size(); j++) {
-				if (artistList.get(i).toLowerCase().contains(simArtistList.get(j))) {
-					System.out.println(artistList.get(i));
-					System.out.println(trackList.get(i));
-					System.out.println();
-				}
-			}
+	}
+	
+	/******************************************************************
+	Displays the list of artist + tracks in String format.
+	@return artist and track title content
+	 *****************************************************************/
+	public String toString() {
+		return released;
 	}
 }
